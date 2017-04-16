67763b426600e47e5ab7a1471e94c5fe250203fd
Eric Moritz <ericmoritz@gmail.com>
2017-04-16 02:34:16 -0400

    calibrated e1 steps

--- Marlin/machines/folgertech_i3_e3d_titan_probe.h
+++ Marlin/machines/folgertech_i3_e3d_titan_probe.h
@@ -46,7 +46,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 418.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
